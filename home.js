4e0db66ed137f80ff2cf0d58419eea481666eb2a
pratyaksh <pratyakshkarmahe@gmail.com>
2025-10-02 10:02:52 +0530

    third commit

--- home.js
+++ home.js
@@ -1,4 +1,18 @@
 var addButton = document.getElementById("add-button");
+
+var tasks = JSON.parse(localStorage.getItem("tasklist"));
+console.log(tasks);
+
+class Task {
+    Task(title, content, date, deadline) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.done = false;
+        this.deadline = deadline;
+    }
+}
+
 addButton.addEventListener("click", () => {
     var title = document.getElementById("title").value;
     var content = document.getElementById("content").value;
@@ -7,4 +21,8 @@ addButton.addEventListener("click", () => {
     console.log(content);
     console.log(title);
     console.log(localStorage.getItem("hello"));
+    var newTask = new Task(title, content, new Date().getFullYear(), deadline);
+    tasks.append(newTask);
+    console.log(tasks);
+    localStorage.setItem("tasklist", JSON.stringify(tasks));
 });
